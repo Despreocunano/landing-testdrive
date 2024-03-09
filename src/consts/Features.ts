b9bdba62fb9c38bb2a0478e39f3cf2607a68589a
Despreocunano <hola@sorostica.com>
2024-03-08 23:25:48 -0300

    cms cards

---
+++ src/consts/Features.ts
@@ -0,0 +1,39 @@
+export const Features = [
+	{
+		icon: "./assets/icons/test.svg",
+        alt: "Icono de un examen",
+		tittle: "Ensayos del examen",
+		description: "Realiza ensayos gratis todos los días o bien accede a ensayos ilimitados durante un mes por sólo $3.990.",
+	},
+    {
+		icon: "./assets/icons/ia.svg",
+        alt: "Icono de un robot de inteligencia artificial",
+		tittle: "Estudia con IA",
+		description: "Asistente virtual que responderá todas tus dudas sobre el examen de conducir y educación vial.",
+	},
+    {
+		icon: "./assets/icons/pencil.svg",
+        alt: "Icono de un lapiz",
+		tittle: "Material de estudio",
+		description: "Encuentra audio libros, glosario vial, señales de tránsito y todo en la palma de tu mano.",
+	},
+    {
+		icon: "./assets/icons/updates.svg",
+        alt: "Icono que representa actualizaciones de contenido",
+		tittle: "Actualizaciones",
+		description: "Una APP que esta al día, agregamos regularmente nuevos contenidos, preguntas y características.",
+	},
+    {
+		icon: "./assets/icons/clock.svg",
+        alt: "Icono de un reljo",
+		tittle: "Examenes rápidos",
+		description: "Pon a prueba tus conocimientos con examenes de 10 preguntas que puedes realizar en cualquier lugar.",
+	},
+    {
+		icon: "./assets/icons/search.svg",
+        alt: "Icono de busqueda",
+		tittle: "Buscador de respuestas",
+		description: "¿Tienes dudas con una pregunta? Utiliza el buscador y encuentra la respuesta y su explicación.",
+	},
+	
+] as const
