4fb79ca42f9ba1907d26353559fd4b1ea423f05c
Despreocunano <hola@sorostica.com>
2024-04-30 12:47:56 -0400

    asd

--- src/consts/Faqs.ts
+++ src/consts/Faqs.ts
@@ -1,31 +1,31 @@
 export const FAQS = [
     {
-        id: 1,
+        isActive: false,
         question: "¿Cuál es el valor de la app?",
         answer: "El costo de la aplicación es de $7.990 por mes o bien $3.990 por 7 días tanto para dispositivos Android e iOS, de igual forma siempre nos encontramos realizando descuentos para la versión premium. Por lo que te invitamos a simular la compra y descubrir si hay algún descuento disponible 🙂",
     },
     {
-        id: 2,
+        isActive: false,
         question: "Cuanto tiempo dura la suscripción",
         answer: "Al momento de realizar la compra se activa tu cuenta premium por 31 o 7 días de acuerdo a la suscripcióm que hayas elegido, es importante que sepas que es un pago recurrente por lo que si no quieres que se renueve deves darte de baja de la suscripción en tu tienda de aplicaciones.",
     },
     {
-        id: 3,
+        isActive: false,
         question: "¿Que pasa si cancelo la suscripción en la tienda de aplicaciones?",
         answer: "Si cancelaste la suscripción no te preocupes, de igual forma seguiras manteniendo el acceso premium por un total de 31 o 7 días según corresponda",
     },
     {
-        id: 4,
+        isActive: false,
         question: "¿El contenido se encuentra actualizado?",
         answer: "Asi es, todo el contenido de la app se encuentra actualizado a la última versión disponible del libro del nuevo conductor, nos encontramos constantemente aumentando nuestra base de preguntas para que te prepares de la mejor manera posible y al mejor precio del mercado.",
     },
     {
-        id: 5,
+        isActive: false,
         question: "¿Cuantas preguntas tienen para practicar el ensayo de la prueba de conducir?",
         answer: "Contamos con más de 1500 preguntas actualizadas y subiendo, además puedes realizar ensayos categorizados dependiento de lo que necesites practicar, ya sea, señales del tránsito, preguntas de selección multiple, verdadero o falso, etc. Animate y descarga nuestra aplicación.",
     },
     {
-        id: 6,
+        isActive: false,
         question: "¿Cómo eliminar mi cuenta en la APP?",
         answer: "Para eliminar la cuenta hay dos caminos: Directo desde tu app debes solicitar la eliminación, esta acción elimina todos los datos personales de nuestra base de datos. Solicitar la eliminación de la cuenta al correo electrónico hola@dreamco.cl y en un plazo de 24 horas todos tus datos serán eliminados.",
     },
